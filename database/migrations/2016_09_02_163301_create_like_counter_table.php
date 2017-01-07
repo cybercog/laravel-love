ab54dc947809cda9f3fdb7758cc109cd76b3a876
ell <ell@cybercog.su>
2017-01-07 05:19:19 +0300

    Update license and docblocks

--- database/migrations/2016_09_02_163301_create_like_counter_table.php
+++ database/migrations/2016_09_02_163301_create_like_counter_table.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Likeable.
  *
- * (c) CyberCog <support@cybercog.su>
+ * (c) Anton Komarev <a.komarev@cybercog.su>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,9 +11,18 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
+/**
+ * Class CreateLikeCounterTable.
+ */
 class CreateLikeCounterTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('like_counter', function (Blueprint $table) {
@@ -34,6 +43,11 @@ class CreateLikeCounterTable extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::drop('like_counter');
--- src/Contracts/Like.php
+++ src/Contracts/Like.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Likeable.
  *
- * (c) CyberCog <support@cybercog.su>
+ * (c) Anton Komarev <a.komarev@cybercog.su>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
