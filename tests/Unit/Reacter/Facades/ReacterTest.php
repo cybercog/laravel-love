9d428dee8bd3dca528816d5b12c4530a0c212d80
antonkomarev <ell@cybercog.su>
2019-06-14 22:17:46 +0300

    Add tests for invalid reaction type name

--- tests/Unit/Reacter/Facades/ReacterTest.php
+++ tests/Unit/Reacter/Facades/ReacterTest.php
@@ -16,6 +16,7 @@ namespace Cog\Tests\Laravel\Love\Unit\Reacter\Facades;
 use Cog\Contracts\Love\Reactant\Exceptions\ReactantInvalid;
 use Cog\Contracts\Love\Reaction\Exceptions\ReactionAlreadyExists;
 use Cog\Contracts\Love\Reaction\Exceptions\ReactionNotExists;
+use Cog\Contracts\Love\ReactionType\Exceptions\ReactionTypeInvalid;
 use Cog\Laravel\Love\Reacter\Facades\Reacter as ReacterFacade;
 use Cog\Laravel\Love\Reacter\Models\Reacter;
 use Cog\Laravel\Love\Reaction\Models\Reaction;
@@ -55,6 +56,18 @@ final class ReacterTest extends TestCase
         $this->assertCount(1, $reacter->reactions);
         $assertReaction = $reacter->reactions->first();
         $this->assertTrue($assertReaction->reactant->reactable->is($reactable));
+    }
+
+    /** @test */
+    public function it_throws_reaction_type_invalid_on_react_to_when_reaction_type_not_exist(): void
+    {
+        $this->expectException(ReactionTypeInvalid::class);
+
+        $reacter = factory(Reacter::class)->create();
+        $reacterFacade = new ReacterFacade($reacter);
+        $reactable = factory(User::class)->create();
+
+        $reacterFacade->reactTo($reactable, 'NotExist');
     }
 
     /** @test */
@@ -115,6 +128,18 @@ final class ReacterTest extends TestCase
 
         $this->assertCount(0, $reacter->reactions);
         $this->assertFalse($reaction->exists());
+    }
+
+    /** @test */
+    public function it_throws_reaction_type_invalid_on_unreact_to_when_reaction_type_not_exist(): void
+    {
+        $this->expectException(ReactionTypeInvalid::class);
+
+        $reacter = factory(Reacter::class)->create();
+        $reacterFacade = new ReacterFacade($reacter);
+        $reactable = factory(Article::class)->create();
+
+        $reacterFacade->unreactTo($reactable, 'NotExist');
     }
 
     /** @test */
@@ -300,6 +325,18 @@ final class ReacterTest extends TestCase
     }
 
     /** @test */
+    public function it_throws_reaction_type_invalid_on_is_reacted_to_with_type_when_reaction_type_not_exist(): void
+    {
+        $this->expectException(ReactionTypeInvalid::class);
+
+        $reacter = factory(Reacter::class)->create();
+        $reacterFacade = new ReacterFacade($reacter);
+        $reactable = factory(Article::class)->create();
+
+        $reacterFacade->isReactedToWithType($reactable, 'NotExist');
+    }
+
+    /** @test */
     public function it_can_check_is_not_reacted_to_reactable_with_type(): void
     {
         $reacter = factory(Reacter::class)->create();
@@ -347,5 +384,17 @@ final class ReacterTest extends TestCase
         $isNotReacted = $reacterFacade->isNotReactedToWithType($reactable, $reactionType->getName());
 
         $this->assertTrue($isNotReacted);
+    }
+
+    /** @test */
+    public function it_throws_reaction_type_invalid_on_is_not_reacted_to_with_type_when_reaction_type_not_exist(): void
+    {
+        $this->expectException(ReactionTypeInvalid::class);
+
+        $reacter = factory(Reacter::class)->create();
+        $reacterFacade = new ReacterFacade($reacter);
+        $reactable = factory(Article::class)->create();
+
+        $reacterFacade->isNotReactedToWithType($reactable, 'NotExist');
     }
 }
