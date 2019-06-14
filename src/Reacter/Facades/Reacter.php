9ad47389cf30ccd2c57fe3678e8df5cf8ba41694
antonkomarev <ell@cybercog.su>
2019-06-14 03:34:03 +0300

    Add ReacterFacade

---
+++ src/Reacter/Facades/Reacter.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * This file is part of Laravel Love.
+ *
+ * (c) Anton Komarev <anton@komarev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Cog\Laravel\Love\Reacter\Facades;
+
+use Cog\Contracts\Love\Reactable\Models\Reactable as ReactableContract;
+use Cog\Contracts\Love\Reacter\Facades\Reacter as ReacterFacadeContract;
+use Cog\Contracts\Love\Reacter\Models\Reacter as ReacterContract;
+use Cog\Contracts\Love\Reacterable\Models\Reacterable as ReacterableContract;
+use Cog\Laravel\Love\ReactionType\Models\ReactionType;
+
+final class Reacter implements ReacterFacadeContract
+{
+    private $reacter;
+
+    public function __construct(ReacterContract $reacter)
+    {
+        $this->reacter = $reacter;
+    }
+
+    public function reactTo(
+        ReactableContract $reactable,
+        string $reactionTypeName
+    ): void {
+        $this->reacter->reactTo(
+            $reactable->getLoveReactant(),
+            ReactionType::fromName($reactionTypeName)
+        );
+    }
+
+    public function unreactTo(
+        ReactableContract $reactable,
+        string $reactionTypeName
+    ): void {
+        $this->reacter->unreactTo(
+            $reactable->getLoveReactant(),
+            ReactionType::fromName($reactionTypeName)
+        );
+    }
+
+    public function isReactedTo(
+        ReactableContract $reactable
+    ): bool {
+        return $this->reacter->isReactedTo(
+            $reactable->getLoveReactant()
+        );
+    }
+
+    public function isNotReactedTo(
+        ReactableContract $reactable
+    ): bool {
+        return $this->reacter->isNotReactedTo(
+            $reactable->getLoveReactant()
+        );
+    }
+
+    public function isReactedToWithType(
+        ReactableContract $reactable,
+        string $reactionTypeName
+    ): bool {
+        return $this->reacter->isReactedToWithType(
+            $reactable->getLoveReactant(),
+            ReactionType::fromName($reactionTypeName)
+        );
+    }
+
+    public function isNotReactedToWithType(
+        ReactableContract $reactable,
+        string $reactionTypeName
+    ): bool {
+        return $this->reacter->isNotReactedToWithType(
+            $reactable->getLoveReactant(),
+            ReactionType::fromName($reactionTypeName)
+        );
+    }
+
+    public function isEqualTo(
+        ReacterableContract $reacterable
+    ): bool {
+        return $this->reacter->isEqualTo(
+            $reacterable->getLoveReacter()
+        );
+    }
+
+    public function isNotEqualTo(
+        ReacterableContract $reacterable
+    ): bool {
+        return $this->reacter->isNotEqualTo(
+            $reacterable->getLoveReacter()
+        );
+    }
+
+    public function isNull(): bool
+    {
+        return $this->reacter->isNull();
+    }
+
+    public function isNotNull(): bool
+    {
+        return $this->reacter->isNotNull();
+    }
+}
---
+++ tests/Unit/Reacter/Facades/ReacterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of Laravel Love.
+ *
+ * (c) Anton Komarev <anton@komarev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Cog\Tests\Laravel\Love\Unit\Reacter\Facades;
+
+use Cog\Laravel\Love\Reacter\Models\Reacter;
+use Cog\Laravel\Love\ReactionType\Models\ReactionType;
+use Cog\Tests\Laravel\Love\Stubs\Models\Article;
+use Cog\Tests\Laravel\Love\Stubs\Models\User;
+use Cog\Tests\Laravel\Love\TestCase;
+
+final class ReacterTest extends TestCase
+{
+    /** @test */
+    public function it_can_react_to_reacterable(): void
+    {
+        $reactionType = factory(ReactionType::class)->create();
+        $article = factory(Article::class)->create();
+        $reacter = factory(Reacter::class)->create([
+            'type' => (new User())->getMorphClass(),
+        ]);
+
+        $reacterable = factory(User::class)->create([
+            'love_reacter_id' => $reacter->getId(),
+        ]);
+
+        $reacterable->akaLoveReacter()->reactTo($article, $reactionType->getName());
+
+        $this->assertCount(1, $reacterable->getLoveReacter()->reactions);
+        $assertReaction = $reacterable->getLoveReacter()->reactions->first();
+        $this->assertTrue($assertReaction->reactant->is($article->getLoveReactant()));
+    }
+}
