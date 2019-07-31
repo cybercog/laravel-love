855eccc3eb0ad04779355f58870a509afdc08665
antonkomarev <ell@cybercog.su>
2019-07-31 04:00:27 +0300

    Update Reaction Change in upgrade command

--- src/Console/Commands/UpgradeV7ToV8.php
+++ src/Console/Commands/UpgradeV7ToV8.php
@@ -14,9 +14,11 @@ declare(strict_types=1);
 namespace Cog\Laravel\Love\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Database\ConnectionInterface;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 final class UpgradeV7ToV8 extends Command
@@ -62,10 +64,23 @@ final class UpgradeV7ToV8 extends Command
         $this->getDbSchema()->table('love_reactions', function (Blueprint $table) {
             $table->decimal('rate', 4, 2)->after('reaction_type_id');
         });
+
+        $this
+            ->getDbQuery()
+            ->table('love_reactions')
+            ->where('rate', 0.0)
+            ->update([
+                'rate' => 1.0,
+            ]);
     }
 
     private function getDbSchema(): Builder
     {
         return Schema::connection(Config::get('love.storage.database.connection'));
+    }
+
+    private function getDbQuery(): ConnectionInterface
+    {
+        return DB::connection(Config::get('love.storage.database.connection'));
     }
 }
