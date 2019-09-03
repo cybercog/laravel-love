51efe852a88d9d1b241e3df733154b1eee2f0da0
Anton Komarev <1849174+antonkomarev@users.noreply.github.com>
2019-09-03 19:38:42 +0300

    Add Laravel v6 support to Love v5 (#114)
    Add Laravel v6 support to Love v5

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -103,9 +103,7 @@ abstract class TestCase extends Orchestra
      */
     protected function migrateUnitTestTables(): void
     {
-        $this->loadMigrationsFrom([
-            '--realpath' => realpath(__DIR__ . '/database/migrations'),
-        ]);
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
 
     /**
@@ -115,9 +113,7 @@ abstract class TestCase extends Orchestra
      */
     protected function migratePackageTables(): void
     {
-        $this->loadMigrationsFrom([
-            '--realpath' => database_path('migrations'),
-        ]);
+        $this->loadMigrationsFrom(database_path('migrations'));
     }
 
     /**
