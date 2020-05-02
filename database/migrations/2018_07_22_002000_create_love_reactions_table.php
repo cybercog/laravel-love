91004a1d11b00ae2deb8b5612946b1451e2bdd4c
antonkomarev <ell@cybercog.su>
2020-05-03 02:53:18 +0300

    Undo deletion of indexes

--- database/migrations/2018_07_22_002000_create_love_reactions_table.php
+++ database/migrations/2018_07_22_002000_create_love_reactions_table.php
@@ -35,6 +35,24 @@ final class CreateLoveReactionsTable extends Migration
             $table->decimal('rate', 4, 2);
             $table->timestamps();
 
+            $table->index([
+                'reactant_id',
+                'reaction_type_id',
+            ]);
+            $table->index([
+                'reactant_id',
+                'reacter_id',
+                'reaction_type_id',
+            ]);
+            $table->index([
+                'reactant_id',
+                'reacter_id',
+            ]);
+            $table->index([
+                'reacter_id',
+                'reaction_type_id',
+            ]);
+
             $table
                 ->foreign('reactant_id')
                 ->references('id')
